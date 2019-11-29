412aca10c69a3cc0c0f77b9358a1cedc65084f4e
Attila Polgar <raglopa@gmail.com>
2019-11-29 16:21:43 +0100

    init

--- scripts/cname.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-fs.writeFile(`dist/CNAME`, "polgar.dev", function(err) {
-  if (err) {
-    console.err(err);
-  } else {
-    console.info("CNAME copied to dist");
-  }
-});
